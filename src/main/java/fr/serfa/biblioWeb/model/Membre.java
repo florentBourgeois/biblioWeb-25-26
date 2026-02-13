7f6195d2d4c67c3d5aaaf1c033c54c6b2b623214
Florent.Bourgeois <florent.bourgeois@uha.fr>
2026-02-13 10:54:01 +0100

    visuels sur l'amélioration

--- src/main/java/fr/serfa/biblioWeb/model/Membre.java
+++ src/main/java/fr/serfa/biblioWeb/model/Membre.java
@@ -3,6 +3,8 @@ package fr.serfa.biblioWeb.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +12,7 @@ import java.util.List;
 public class Membre {
 
     private String nom;
-    private String dateAbonnement;
+    private String dateAbonnement = "";
     private List<Livre> emprunts = new ArrayList<>();
 
     public Membre(String nom, String dateAbonnement) {
@@ -18,6 +20,22 @@ public class Membre {
         this.dateAbonnement = dateAbonnement;
     }
 
+    public void emprunter(Livre l){
+        this.emprunts.add(l);
+    }
+
+    public void rendre(Livre l){
+        this.emprunts.remove(l);
+    }
+
+    public void payerAbonnement(){
+        this.dateAbonnement = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    }
+
+    public boolean estAbonne(){
+        LocalDate dateAbonnement = LocalDate.parse(this.dateAbonnement, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        return dateAbonnement.plusYears(1).isAfter(LocalDate.now());
+    }
     @JsonIgnore
     public List<Livre> getEmprunts() {
         return emprunts;
