425442f8ccbd9e1fc0525fdc88d13c8d7ae758fa
Florent.Bourgeois <florent.bourgeois@uha.fr>
2026-02-20 14:54:59 +0100

    ajout de commentaires pour lisibilité

--- src/main/java/fr/serfa/biblioWeb/dao/LivreDAO.java
+++ src/main/java/fr/serfa/biblioWeb/dao/LivreDAO.java
@@ -11,9 +11,7 @@ public interface LivreDAO extends JpaRepository<Livre, Long> {
     List<Livre> findByTitreContains(String titre);
 
     @Query("select l from Livre l where l.id not in (select li.id from Membre m join m.emprunts li)")
-    List<Livre> findLivresDisponibles();
-
-
+    List<Livre> findLivresDisponibles(); // non empruntés
 
 
 }
--- src/main/java/fr/serfa/biblioWeb/dao/MembreDAO.java
+++ src/main/java/fr/serfa/biblioWeb/dao/MembreDAO.java
@@ -15,9 +15,18 @@ public interface MembreDAO extends JpaRepository<Membre, Long> {
 
     List<Membre> findByEmprunts_IdNotNull();
 
+    // tous les emprunts d'un membre
     @Query("select emprunts from Membre where id=:id")
     List<Livre> findEmpruntById(Long id);
 
     // trouver l'emprunteur d'un livre
+    Membre findByEmprunts_Id(Long id);
+
+    // trouver les emprunteurs de livres dont l'auteur est id
+    List<Membre> findByEmprunts_Auteur_Id(Long id);
+
+    // trouver les emprunteurs de livres dont l'auteur a rédigé un livre contenant le titre
+    List<Membre> findByEmprunts_Auteur_Livres_Titre(String titre);
+
 
 }
